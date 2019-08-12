c1b88e62566b0ebe759b0cbca4ad51240941341d
ashu961 <beheraashutosh5@gmail.com>
2019-08-12 13:24:13 +0530

    client added

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ require("./routes/billingRoutes")(app);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require("path");
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
